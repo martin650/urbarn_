e0d30535791e0abd9a05fb1e07cbb7a1da42533f
martin <99643254+martin650@users.noreply.github.com>
2024-08-06 11:18:57 +0300

    Add files via upload

---
+++ Medicare_System/includes/footer.php
@@ -0,0 +1,56 @@
+<footer>
+	<div class="footer-logo">
+		EQUITYAFIA<i class="fas fa-stethoscope"></i>
+	</div>
+	<div class="social">
+		<a href="#"><i class="fab fa-facebook"></i></a>
+		<a href="#"><i class="fab fa-twitter"></i></a>
+		<a href="#"><i class="fab fa-instagram"></i></a>
+		<a href="#"><i class="fab fa-whatsaap"></i></a>
+		<a href="#"><i class="fab fa-linkedin"></i></a>
+	</div>
+	<div class="container">
+		<div class="footer-cont">
+			<h4>ABOUT US</h4>
+			<a href="./aboutus.php">Company</a>
+			<a href="./reviews.php">Reviews</a>
+			<a href="./management.php">Stakeholders</a>
+			<a href="./award.php">Monthly Awards</a>
+		</div>
+		<div class="footer-cont">
+			<h4>QUICK LINKS</h4>
+			<a href="./services.php">Services</a>
+			<a href="./patient_signup.php">create account</a>
+			<a href="./doc_signin.php">Login</a>
+			<a href="./admin/index.php">Administrator</a>
+		</div>
+		<div class="footer-cont">
+			<h4>MAIN MENU</h4>
+			<a href="./blog.php">Blogs</a>
+			<a href="./patient_signup.php">Patient account</a>
+			<a href="./gallery.php">Gallery</a>
+			<a href="./doc_signup.php">Doctors Account</a>
+		</div>
+		<div class="footer-cont">
+			<h4>Newsletter</h4>
+			<p>Follow us on social media for latest updates.</p>
+			<form action="./resources/subscribe.php" method="post">
+			<div class="footer-newsletter">
+					<input type="email" name="email" placeholder="Enter email" required>
+					<button name="subscribe">Send</button>
+			</div>
+		</form>
+		</div>
+	</div>
+	<hr class="divide">
+		<div class="copy">
+			<div class="copy-left">
+				&copy;2022 ALL RIGHTS RESERVED
+			</div>
+			<div class="copy-right">
+				<a href="#">Terms</a>
+				<a href="#">Conditions</a>
+				<a href="#">Policy</a>
+			</div>
+		</div>
+</footer>
---
+++ Medicare_System/includes/navbar.php
@@ -0,0 +1,17 @@
+<div class="header">
+	<a href="#" class="name"><i class="fas fa-medkit"></i>AFIACARE</a>
+
+	<nav class="navbar">
+		<a href="./index.php" class="active"><i class="fas fa-h-square"></i>Home</a>
+		<a href="./services.php"><i class="fas fa-stethoscope"></i>Services</a>
+		<a href="./aboutus.php"><i class="fas fa-briefcase-medical"></i>About Us</a>
+		<a href="./contactus.php"><i class="far fa-address-book"></i>Contact us</a>
+		<a href="./remedies.php"><i class="fas fa-hand-holding-medical"></i>Remedies</a>
+	</nav>
+
+	<div class="right">
+		<a href="./patient_signup.php"><div class="far fa-user" id=""></div></a>
+		<a href="./doc_signup.php"><div class="fas fa-user-md" id=""></div></a>
+		<div class="fas fa-bars" id="menu"></div>
+	</div>
+</div>
